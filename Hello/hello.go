72f34c92121aa489e096b105dd24936d0da0982a
Rafael Cena <rafael.cena@hotmail.com>
2023-01-28 08:32:41 -0300

    entrada de input terminal

--- Hello/hello.go
+++ Hello/hello.go
@@ -1,9 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Olá Mundo")
+	nome := "Rafael"
+	versao := 1.1
+	fmt.Println("Olá sr, ", nome)
+	fmt.Println("Este programa está na versão ", versao)
+
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do Programa")
+
+	var comando int
+
+	fmt.Scanf("%d", &comando)
+	fmt.Println("O endereço de memória de comando é ", &comando)
+	fmt.Println("O comando escolhido foi ", comando)
+
+	// outra forma
+	var comando2 int
+	fmt.Scan(&comando2)
+	fmt.Println("O endereço de memória de comando2 é ", &comando2)
+	fmt.Println("O comando escolhido foi ", comando2)
 }
 
 /* ## Compilando o programa
