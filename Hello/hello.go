f36c80d9fcabed75b1ec05069d2bd280f283d3fc
Rafael Cena <rafael.cena@hotmail.com>
2023-01-28 08:55:38 -0300

    funções basicas

--- Hello/hello.go
+++ Hello/hello.go
@@ -5,20 +5,11 @@ import (
 )
 
 func main() {
-	nome := "Rafael"
-	versao := 1.1
-	fmt.Println("Olá sr, ", nome)
-	fmt.Println("Este programa está na versão ", versao)
+	exibeIntroducao()
 
-	fmt.Println("1- Iniciar Monitoramento")
-	fmt.Println("2- Exibir Logs")
-	fmt.Println("0- Sair do Programa")
+	exibeMenu()
 
-	var comando int
-
-	fmt.Scanf("%d", &comando)
-	fmt.Println("O endereço de memória de comando é ", &comando)
-	fmt.Println("O comando escolhido foi ", comando)
+	comando := leComando()
 
 	switch comando {
 	case 1:
@@ -29,9 +20,30 @@ func main() {
 		fmt.Println("Saindo do programa...")
 	default:
 		fmt.Println("Não conheço este comando")
-
 	}
 
+}
+
+func exibeIntroducao() {
+	nome := "Rafael"
+	versao := 1.1
+	fmt.Println("Olá sr, ", nome)
+	fmt.Println("Este programa está na versão ", versao)
+}
+
+func leComando() int {
+	var comando int
+	fmt.Scanf("%d", &comando)
+	fmt.Println("O endereço de memória de comando é ", &comando)
+	fmt.Println("O comando escolhido foi ", comando)
+
+	return comando
+}
+
+func exibeMenu() {
+	fmt.Println("1 - Iniciar Monitoramento")
+	fmt.Println("2 - Exibir Logs")
+	fmt.Println("0 - Sair do Programa")
 }
 
 /* ## Compilando o programa
