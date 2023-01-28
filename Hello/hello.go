32402168eb8e4dc901f44869841722299c09d5d9
Rafael Cena <rafael.cena@hotmail.com>
2023-01-28 08:58:08 -0300

    funções de sair

--- Hello/hello.go
+++ Hello/hello.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
@@ -18,8 +19,11 @@ func main() {
 		fmt.Println("Exibindo Logs...")
 	case 0:
 		fmt.Println("Saindo do programa...")
+		os.Exit(0)
+
 	default:
 		fmt.Println("Não conheço este comando")
+		os.Exit(-1)
 	}
 
 }
