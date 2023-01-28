758c82bc4421481d68aedb40c674605937fc8571
Rafael Cena <rafael.cena@hotmail.com>
2023-01-28 08:09:19 -0300

    hello world

---
+++ Hello/hello.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Olá Mundo")
+}
+
+/* ## Compilando o programa
+## go build hello.go
+## ./hello
+## copilando e rodando
+# go run hello.go
+*/
