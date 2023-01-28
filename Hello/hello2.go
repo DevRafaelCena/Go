e79bc94d9f44528b8bc585d26cd46fc0e63a389d
Rafael Cena <rafael.cena@hotmail.com>
2023-01-28 08:19:50 -0300

    tipagem

--- Hello/hello2.go
+++ Hello/hello2.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
 	var nome string = "Rafael"
@@ -8,6 +11,10 @@ func main() {
 	var versao float32 = 1.1
 	fmt.Println("Olá sr, ", nome, "sua idade é ", idade)
 	fmt.Println("Este programa esta na versão ", versao)
+
+	fmt.Println("O tipo da variavel nome é ", reflect.TypeOf(nome))
+	fmt.Println("O tipo da variavel idade é ", reflect.TypeOf(idade))
+	fmt.Println("O tipo da variavel versao é ", reflect.TypeOf(versao))
 }
 
 /* ## Compilando o programa
